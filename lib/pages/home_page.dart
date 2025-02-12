ec834bd8a90e07202c223d416b22dc34731c6490
Hector-Pat <enderpat223@gmail.com>
2025-02-12 13:22:19 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,9 +17,42 @@ class _HomePageState extends State<HomePage> {
         color:Colors.grey[800]
         ),
         actions:[
-          Icon(Icons.person)
+          Padding(
+            padding: const EdgeInsets.only(right:24.0),
+            child: Icon(Icons.person),
+          )
         ]
-      )
+      ),
+      body: const Column(
+        children: [
+          //Texto principal
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+            child: Row(
+              children: [
+                Text("I want to ",
+                style: TextStyle(fontSize:32),),
+                    
+                Text("Eat",
+                style: TextStyle(
+                  //tamaño de letra
+                  fontSize:32,
+                  //negritas
+                  fontWeight: FontWeight.bold,
+                  //subrayado
+                  decoration: TextDecoration.underline))
+                  
+                
+              ],
+            ),
+          ),
+          
+          // pestañas/tapbar
+
+          // contenido/tapbarview
+
+          //Carrito/cart
+        ],)
     );
   }
 }
