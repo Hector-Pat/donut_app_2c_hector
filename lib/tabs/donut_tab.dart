69fb20cc61602f2f4935ebb3918ae0d2d897c4ef
Hector-Pat <enderpat223@gmail.com>
2025-03-07 12:39:09 -0600

    PriceTag Added

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -1,44 +1,39 @@
+import 'package:donut_app_2c_hector/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class DonutTab extends StatelessWidget {
-
-  // lista de donas
-    final List donutsOnSale = [
-
-   // [ donutFlavor, donutPrice, donutColor, imageName ]
-
-   ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-
-   ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-
-   ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-
-   ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-
-];
+  //Lista de donas
+  final List donutsOnSale = [
+    // [ donutFlavor, donutPrice, donutColor, imageName ]
+    
+    ["Ice Cream", "36", Colors.deepOrange, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+  ];
   DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Widget para usar cuadricula
+    //Widget para usar cuadrícula
     return GridView.builder(
-      //cuantos elementos hay en una cuadricula
-      itemCount: donutOnSale.length,
-      padding: EdgeInsets.all(12),
-      //organiza como se veran los elementos
-      gridDelegate: ,
-      SliverGridDelegateWithFixedCrossAxisCount(
-        //cuantas columnas
-        crossAxisCount: 2
-      )
-      ItemBuilder:(context,index){
-        //cada elemento individual
-        return DonutTile(
-          donutFlavor
-        )
-        donutFlavor: donutsOnSale[index][0]
-      }
-
-    )
+        //Cuántos elementos hay
+        itemCount: donutsOnSale.length,
+        padding: EdgeInsets.all(12),
+        //cómo se distrubuirán los elementos
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //Cuántas columnas
+          crossAxisCount: 2,
+          //Relación de aspecto(proporción)
+          childAspectRatio: 1 / 1.5,
+        ),
+        itemBuilder: (context, index) {
+          return DonutTile(
+            donutFlavor: donutsOnSale[index][0],
+            donutPrice: donutsOnSale[index][1],
+            donutColor: donutsOnSale[index][2],
+            imageName: donutsOnSale[index][3],
+          );
+        });
   }
 }
